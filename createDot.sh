8a5bd5a84940bfc05490c5596d1f1abfb3b84c85
Michael Beebe <michael.beebe@ttu.edu>
2023-04-03 14:58:14 -0500

    updated createDot.sh

--- createDot.sh
+++ createDot.sh
@@ -5,7 +5,16 @@
 #
 
 # install the necessary npm modules
+npm install
 
 # create .dot file
+if [ -f "CFG.dot" ]; then
+  rm "CFG.dot"
+fi
+if [ -f "CFG.png" ]; then
+  rm "CFG.png"
+fi
 npx ts-node controlFlowGraph.ts
+
+# create the .png from the .dot file
 dot -Tpng CFG.dot -o CFG.png
