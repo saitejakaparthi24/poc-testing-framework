6f0a4a1e9542a8b01b0c9be70745bb82fe60cc47
saitejakaparthi24 <114630408+saitejakaparthi24@users.noreply.github.com>
2023-04-07 01:27:37 -0500

    Test Commit

--- src/data/usecases/entity/pet.ts
+++ src/data/usecases/entity/pet.ts
@@ -8,4 +8,28 @@ export class      Pet {
       return `${this.name}says:${sound}raaar!`
     }
   }
+
+  intro(sound:string): string {
+    let crySound = '';
+
+    switch (this.breed) {
+      case 'cavapoo':
+        crySound = `${this.name} says: ${sound}!`;
+        break;
+      case 'canine':
+            crySound = `${this.name} says: ${sound}!`;
+            break;
+      case 'poodles':
+            crySound = `${this.name} says: ${sound}!`;
+            break;
+      case 'bulldogs':
+            crySound = `${this.name} says: ${sound}!`;
+            break;
+      default:
+        crySound = `${this.name} says: ${sound} default sound!`;
+        break;
+    }
+
+    return crySound;
+  }
 }
